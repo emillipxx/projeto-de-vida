7154c8db84d1cf6928eaab1cae5dc55ad3c37c9f
emillipxx <168201410+emillipxx@users.noreply.github.com>
2026-03-12 12:06:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.lenght;i++){
-    console.log(botoes[i]);
+console.log(botoes);
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        botoes[i].classList.add("ativo");
+    }
+    console.log(i)
 }
